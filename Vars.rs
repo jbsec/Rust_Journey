eb5d4b5c19940b7df3e1ee7bc98c62a6b1269b97
James Barrett AfCIIS <45885505+jbsec@users.noreply.github.com>
2021-10-12 23:14:07 +0100

    Add files via upload
    Some var stuff, been busy with work so uploads have taken a bit of a hit here and there

---
+++ Vars.rs
@@ -0,0 +1,37 @@
+//let mut variable_name = value;
+//let mut variable_name:dataType = value;
+// why the muts not working? errors thrown
+fn main() {
+    //println!("Hello, world!");
+    //vars bit
+    //let value = 24;
+   // let variable_name = value; // no type specified
+    // let variable_name:int = value; // type specified
+    
+    /*
+    let fees = 25_000;
+    let salary:f64 = 35_000.00;
+    println!("fees is {} and salary is {}",fees,salary);
+    //immutable
+    let fees2 = 25_000;
+    println!("fees in {} ",fees2);
+    fees2 = 35_000;
+    println!("fee changed is {}",fees2);
+    */
+    //mutable here
+    //trying to put vars in fun
+    /*
+    let mut variable_name = value;
+    let mut variable_name:dataType = value;
+    */
+    
+    let mut fees:i32 = 25_000;
+    println!("fees is {} ",fees);
+    fees = 35_000;
+    println!("fees changed is {}",fees);
+    
+}
+
+// have to refamiliarise myself here
+// taken a short break from coding.
+
